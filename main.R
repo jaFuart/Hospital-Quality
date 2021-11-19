7a3706a729847a35650fd949039440f034a4b399
jaFuart <kl.yz.vw1337@gmail.com>
2021-11-19 10:19:50 +0300

    Created function that's ranking hospitals in that state for that outcome

--- main.R
+++ main.R
@@ -16,12 +16,12 @@ names(outcome)
 outcome[, 11] <- as.numeric(outcome[, 11])
 hist(outcome[, 11])
 
-##Source function
+##Source best function
 source("best.R")
 
 ##Making some examples
 best("TX", "heart attack")
-## "CYPRESS FAIRBANKS MEDICAL CENTER"
+##"CYPRESS FAIRBANKS MEDICAL CENTER"
 best("TX", "heart failure")
 ##"FORT DUNCAN MEDICAL CENTER"
 best("MD", "heart attack")
@@ -32,3 +32,14 @@ best("BB", "heart attack")
 ##Error in best("BB", "heart attack") : invalid state
 best("NY", "hert attack")
 ##Error in best("NY", "hert attack") : invalid outcome
+
+##Source rankhospital function
+source("rankhospital.R")
+
+##Making some examples
+rankhospital("TX", "heart failure", 4)
+##"DETAR HOSPITAL NAVARRO"
+rankhospital("MD", "heart attack", "worst")
+##"HARFORD MEMORIAL HOSPITAL"
+rankhospital("MN", "heart attack", 5000)
+##NA
--- rankhospital.R
+++ rankhospital.R
@@ -1,15 +1,17 @@
-best <- function(state, outcome) {
+rankhospital <- function(state, outcome, num = 'best') {
+  
   ##Read outcome data
+  
   data <- read.csv(file='outcome-of-care-measures.csv', 
                    colClasses = 'character', 
                    na.strings = c("NA", "Not Available"))
   
   ##Get data we're interested in
   
-  rates <- as.data.frame(cbind(data[, 2],   #hospital 
+  rates <- as.data.frame(cbind(data[, 2],   #hospital
                                data[, 7],   #state
-                               data[, 11],  #heart attack 
-                               data[, 17],  #heart failure 
+                               data[, 11],  #heart attack
+                               data[, 17],  #heart failure
                                data[, 23]), #pneumonia
                          stringsAsFactors = FALSE)
   
@@ -22,6 +24,7 @@ best <- function(state, outcome) {
                        "pneumonia")
   
   ##Check that state and outcome are valid
+  
   if(!state %in% rates[,"state"]){
     stop('invalid state')
   }
@@ -30,23 +33,32 @@ best <- function(state, outcome) {
     stop('invalid outcome')
   }
   
-  ##Return hospital name in that state with lowest 30-day death rate
+  
+  ##Return hospital name in that state with lowest 30-day death
+  ##rate
   
   ##Get only the hospitals in chosen state
   nRates <- rates[(rates[, "state"] == state), ]
   
-  ##Convert outcome rate to numeric
+  ##Convert outcome rate to numberic, gets a warning
   nRates[, outcome] <- as.numeric(nRates[, outcome])
   
   ##Remove NA values
   nRates <- nRates[!is.na(nRates[, outcome]), ]
   
+  ##convert num argument to valid rank
+  
+  if(num == "best") {
+    num <- 1 
+  } else if (num == "worst") {
+    num <- nrow(nRates) 
+  }
+  
   ##Order by outcome rate
-  nRates <- nRates[order(nRates[, outcome]), ]
+  nRates <- nRates[order(nRates[, outcome], nRates[, "hospital"]), ]
   
-  ##Get names of hosptial with the lowest rate
-  eNames <- nRates[nRates[, outcome] == min(nRates[,outcome]),1]
+  ##Get names of hospital 
   
-  ##Sort by hospital name if tie
-  sort(eNames)[1]
+  nRates[num,1]
+  
 }
